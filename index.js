da29268df5eb4ef41a141cb2b3a9e3ba212d6a3a
Pavel Yashchenko <yashchenkops@gmail.com>
2022-09-25 18:46:47 +0300

    home work

---
+++ index.js
@@ -0,0 +1,32 @@
+const store = {
+  name: 'Minimarket',
+  address: {
+    country: 'Ukraine',
+    city: 'Kyiv',
+    street: 'Soborna st.',
+    houseNumber: 43
+  }
+}
+
+store.clients = {
+  "Bohdan Onatskiy": {
+    quantity: 5,
+    bonuses: 26
+  },
+  "Oleksii Medvediev": {
+    quantity: 7,
+    bonuses: 53
+  },
+  "Nataliia Kulybina": {
+    quantity: 3,
+    bonuses: 5
+  },
+  "Yevhen Vuzlov": {
+    quantity: 14,
+    bonuses: 122
+  },
+  "Alina Kholod": {
+    quantity: 9,
+    bonuses: 56
+  }
+}
